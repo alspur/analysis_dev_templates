35663ead747ffd97fc62d22ce92e174ffcd7e56b
Alex S <alex.spurrier@gmail.com>
2017-07-11 08:29:37 -0400

    create analysis_template.R

---
+++ analysis_template.R
@@ -0,0 +1,26 @@
+# title of analysis
+#
+# date started (yyyy-mm-dd)
+# lead analysis developer
+#
+# notes re: purpose of project
+
+# load ####
+
+# load packages 
+
+# load raw data
+
+
+# clean ####
+
+# prepare data in a tidy format
+
+
+# analyze ####
+
+# transform, model, and/or visualize data
+
+# communicate ####
+
+# produce objects (tables, charts, etc) for communcating results
